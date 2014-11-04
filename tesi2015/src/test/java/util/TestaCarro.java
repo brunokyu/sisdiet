bce3a7dff697d7c654af6795ca9200b5e9132358
juniorlimeira <juniorlimeiras@gmail.com>
2014-11-04 11:40:28 -0200

    Add comentários na classe TestaCarro.

--- tesi2015/src/test/java/util/TestaCarro.java
+++ tesi2015/src/test/java/util/TestaCarro.java
@@ -5,7 +5,7 @@ import org.hibernate.Session;
 public class TestaCarro {
 
 	public static void main(String[] args) {
-	
+//		criação da sessão
 		Session s = HibernateUtil.getSessionFactory().openSession();
 		s.beginTransaction();
 		Carro c = new Carro();
