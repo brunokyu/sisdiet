7e0025d0e63b0dabf3163ac9f9eca7ccadb419bb
Bruno Tesi2 <Bruno Tesi2@172.16.108.69>
2015-01-08 13:02:06 -0500

    alteração nas classes entidades

--- sisdiet/src/test/java/entity/Alimentos.java
+++ sisdiet/src/test/java/entity/Alimentos.java
@@ -1,5 +1,24 @@
 package entity;
 
-public class Alimentos {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class Alimentos implements Serializable {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer idAlimento;
+	@Column(nullable=false)
+	private String NomeAlimento;
+	@Column
+	private String DescAlimento;
+	@Column
+	private double QuantAlimento;
+	
 }
--- sisdiet/src/test/java/entity/Usuario.java
+++ sisdiet/src/test/java/entity/Usuario.java
@@ -1,5 +1,22 @@
 package entity;
 
-public class Usuario {
+import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Usuario implements Serializable{
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Integer idUser;
+	@Column(nullable=false)
+	private String NomeUser;
+	@Column
+	private String Senha;
+	@Column
+	private int nivel;
 }
