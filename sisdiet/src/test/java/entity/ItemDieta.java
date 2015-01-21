e22b72114924cbef1c2c09e9c1333c667c00013f
Bruno Tesi2 <Bruno Tesi2@172.16.108.69>
2015-01-21 14:49:28 -0500

    Gerencia dieta e itemdieta

--- sisdiet/src/test/java/entity/ItemDieta.java
+++ sisdiet/src/test/java/entity/ItemDieta.java
@@ -14,6 +14,13 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
 
+@Entity
 public class ItemDieta implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+@Column
+private int idUser;
+@Column
+private int idLista;
 
 }
